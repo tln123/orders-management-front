4e0220cd552d2c8b4b91257ee30ef5ec049159bd
Tom <tln123fk@gmail.com>
2021-02-15 20:48:28 +0200

    Updates

---
+++ precache-manifest.292772f357f2f67f81728ae4f5ea82ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edff5014a074933b9dcbe56c9b2f01d",
+    "url": "/orders-management-front/index.html"
+  },
+  {
+    "revision": "48f7c685f36999d15600",
+    "url": "/orders-management-front/static/css/main.cfcfa1ea.chunk.css"
+  },
+  {
+    "revision": "a806e284275058a7644a",
+    "url": "/orders-management-front/static/js/2.5715578b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/orders-management-front/static/js/2.5715578b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f7c685f36999d15600",
+    "url": "/orders-management-front/static/js/main.76120f2a.chunk.js"
+  },
+  {
+    "revision": "daf0d65ac7934bb52c0a",
+    "url": "/orders-management-front/static/js/runtime-main.63ed3dca.js"
+  }
+]);
